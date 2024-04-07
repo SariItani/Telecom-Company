17a4d144d1336395250f2807cf0f823e62d62163
SariItani <74187768+SariItani@users.noreply.github.com>
2024-04-07 17:32:50 +0300

    Update telecom.sql

--- phase2_files/telecom.sql
+++ phase2_files/telecom.sql
@@ -3,108 +3,108 @@ CREATE DATABASE TELECOM;
 USE TELECOM;
 
 CREATE TABLE Customers (
-    cid INT NOT NULL AUTO_INCREMENT,
-    Customer_Name VARCHAR(50) NOT NULL,
-    ContactInfo VARCHAR(50) NOT NULL,
-    Addr VARCHAR(50) NOT NULL,
-    PRIMARY KEY (cid)
+  cid INT NOT NULL AUTO_INCREMENT,
+  customer_name VARCHAR(50) NOT NULL,
+  contact_info VARCHAR(50) NOT NULL,
+  customer_address VARCHAR(50) NOT NULL,
+  PRIMARY KEY (cid)
 );
 
 CREATE TABLE Accounts (
-    aid INT NOT NULL AUTO_INCREMENT,
-    cid INT NOT NULL,
-    AccountType ENUM('Individual', 'Business'),
-    Account_Status ENUM('Active', 'Inactive'),
-    PRIMARY KEY (aid),
-    FOREIGN KEY (cid) REFERENCES Customers(cid)
+  aid INT NOT NULL AUTO_INCREMENT,
+  cid INT NOT NULL,
+  account_type ENUM('Individual','Business'),
+  account_status ENUM('Active','Inactive'),
+  PRIMARY KEY (aid),
+  FOREIGN KEY (cid) REFERENCES Customers(cid)
 );
 
 CREATE TABLE Services (
-    pid INT NOT NULL AUTO_INCREMENT,
-    Serive_Name VARCHAR(50) NOT NULL,
-    Describe VARCHAR(150) NOT NULL,
-    Price DECIMAL(10, 2) NOT NULL,
-    PRIMARY KEY (pid)
+  pid INT NOT NULL AUTO_INCREMENT,
+  service_name VARCHAR(50) NOT NULL,
+  description VARCHAR(50) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (pid)
 );
 
 CREATE TABLE SIM_Cards (
-    IMSI VARCHAR(50) NOT NULL,
-    aid INT, -- Could be NULL, if it was not linked to an account, but the card itself still exists.
-    Phone_Number VARCHAR(50) NOT NULL,
-    SIM_Status ENUM('Active', 'Inactive'),
-    ICCID VARCHAR(50) NOT NULL,
-    PUK VARCHAR(50) NOT NULL,
-    PIN VARCHAR(50) NOT NULL,
-    PRIMARY KEY (IMSI),
-    UNIQUE (ICCID),
-    UNIQUE (PUK),
-    UNIQUE (PIN),
-    FOREIGN KEY (aid) REFERENCES Accounts(aid)
+  IMSI VARCHAR(50) NOT NULL,
+  aid INT, -- Could be NULL if the SIM Card exists but is not linked to any account yet.
+  phone_number VARCHAR(50) NOT NULL,
+  sim_status ENUM('Active', 'Inactive'),
+  ICCID VARCHAR(50) NOT NULL,
+  PUK VARCHAR(50) NOT NULL,
+  PIN VARCHAR(50) NOT NULL,
+  PRIMARY KEY (IMSI),
+  UNIQUE (ICCID),
+  UNIQUE (PUK),
+  UNIQUE (PIN),
+  FOREIGN KEY (aid) REFERENCES Accounts(aid)
 );
 
 CREATE TABLE Subscriptions (
-    sub_id INT NOT NULL AUTO_INCREMENT,
-    pid INT NOT NULL,
-    IMSI VARCHAR(50) NOT NULL,
-    Starting_Date DATE NOT NULL,
-    Ending_Date DATE NOT NULL,
-    Renewal ENUM('Auto', 'Manual'),
-    PRIMARY KEY (sub_id),
-    FOREIGN KEY (pid) REFERENCES Services(pid),
-    FOREIGN KEY (IMSI) REFERENCES SIM_Cards(IMSI)
+  sub_id INT NOT NULL AUTO_INCREMENT,
+  pid INT NOT NULL,
+  IMSI VARCHAR(50) NOT NULL,
+  starting_date DATE NOT NULL,
+  ending_date DATE NOT NULL,
+  renewal ENUM('Auto', 'Manual'),
+  PRIMARY KEY (sub_id),
+  FOREIGN KEY (pid) REFERENCES Services(pid),
+  FOREIGN KEY (IMSI) REFERENCES SIM_Cards(IMSI)
 );
 
 CREATE TABLE Support_Tickets (
-    tid INT NOT NULL AUTO_INCREMENT,
-    aid INT NOT NULL,
-    IssueDescription VARCHAR(150) NOT NULL,
-    Ticket_Status ENUM('Complete', 'Incomplete'),
-    ResolutionDetails VARCHAR(150),
-    PRIMARY KEY (tid),
-    FOREIGN KEY (aid) REFERENCES Accounts(aid)
+  tid INT NOT NULL AUTO_INCREMENT,
+  aid INT NOT NULL,
+  issue_description VARCHAR(50) NOT NULL,
+  ticket_status ENUM('Active', 'Inactive'),
+  resolution_details VARCHAR(50),
+  PRIMARY KEY (tid),
+  FOREIGN KEY (aid) REFERENCES Accounts(aid)
 );
 
 CREATE TABLE Employees (
-    eid INT NOT NULL AUTO_INCREMENT,
-    Employee_Name VARCHAR(50) NOT NULL,
-    ContactInfo VARCHAR(50) NOT NULL,
-    Addr VARCHAR(50) NOT NULL,
-    Department ENUM('POS', 'Site', 'Warehouse'),
-    Job_Title VARCHAR(50) NOT NULL,
-    PRIMARY KEY (eid)
+  eid INT NOT NULL AUTO_INCREMENT,
+  employee_name VARCHAR(50) NOT NULL,
+  contact_info VARCHAR(50) NOT NULL,
+  employee_address VARCHAR(50) NOT NULL,
+  department ENUM('POS', 'Site', 'Warehouse'),
+  job_title VARCHAR(50) NOT NULL,
+  PRIMARY KEY (eid)
 );
 
 CREATE TABLE Payments (
-    pay_id INT NOT NULL AUTO_INCREMENT,
-    aid INT NOT NULL,
-    eid INT,
-    sub_id INT,
-    Due_Date DATE NOT NULL,
-    Amount DECIMAL(10, 2) NOT NULL,
-    Method ENUM('Credit Card', 'Cash'),
-    Done_Date DATE,
-    PRIMARY KEY (pay_id),
-    FOREIGN KEY (aid) REFERENCES Accounts(aid),
-    FOREIGN KEY (eid) REFERENCES Employees(eid),
-    FOREIGN KEY (sub_id) REFERENCES Subscriptions(sub_id)
+  payment_id INT NOT NULL AUTO_INCREMENT,
+  aid INT NOT NULL,
+  eid INT,
+  sub_id INT,
+  due_date DATE NOT NULL,
+  amount DECIMAL(10,2) NOT NULL,
+  payment_method ENUM('Credit Card', 'Cash'),
+  payment_date DATE,
+  PRIMARY KEY (payment_id),
+  FOREIGN KEY (aid) REFERENCES Accounts(aid),
+  FOREIGN KEY (eid) REFERENCES Employees(eid),
+  FOREIGN KEY (sub_id) REFERENCES Subscriptions(sub_id)
 );
 
 CREATE TABLE Departments (
-    did INT NOT NULL AUTO_INCREMENT,
-    eid INT NOT NULL,
-    Describe VARCHAR(150),
-    Capacity INT,
-    Addr VARCHAR(50) NOT NULL,
-    Departmetn_Name ENUM('POS', 'Site', 'Warehouse'),
-    PRIMARY KEY (did),
-    FOREIGN KEY (eid) REFERENCES Employees(eid)
+  did INT NOT NULL AUTO_INCREMENT,
+  eid INT NOT NULL,
+  department_description VARCHAR(50),
+  capacity INT,
+  department_address VARCHAR(50) NOT NULL,
+  department_name ENUM('POS', 'Site', 'Warehouse'),
+  PRIMARY KEY (did),
+  FOREIGN KEY (eid) REFERENCES Employees(eid)
 );
 
 CREATE TABLE Equipment (
-    eqid INT NOT NULL AUTO_INCREMENT,
-    Equipment_Name VARCHAR(50) NOT NULL,
-    Model VARCHAR(50) NOT NULL,
-    Department ENUM('POS', 'Site', 'Warehouse'),
-    Equipment_Status ENUM('Active', 'Inactive'),
-    PRIMARY KEY (eqid)
+  eqid INT NOT NULL AUTO_INCREMENT,
+  equipment_name VARCHAR(50) NOT NULL,
+  model VARCHAR(50) NOT NULL,
+  department ENUM('POS', 'Site', 'Warehouse'),
+  equipment_status ENUM('Active', 'Inactive'),
+  PRIMARY KEY (eqid)
 );
