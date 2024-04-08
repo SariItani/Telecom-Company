6866a27736fe418313dfe86664bb98220b8dfcc7
SariItani <74187768+SariItani@users.noreply.github.com>
2024-04-08 21:10:11 +0300

    logosss

--- phase3_files/telecom_app/views.py
+++ phase3_files/telecom_app/views.py
@@ -6,3 +6,11 @@ def index():
     cursor.execute("SELECT * FROM Customers")
     customers = cursor.fetchall()
     return render_template('index.html', customers=customers)
+
+@app.route('/employees')
+def EPortal():
+    # require login
+    # query employee entry from Employees table and then
+    # username = employee.username
+    # send usermame to frontend by adding username=username to the render_template()
+    return render_template('index-employees.html')
